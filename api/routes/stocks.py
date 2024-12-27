7c4d1ee001a0a2bd4a4b89d75a95a4f4011ca1d9
Sinkovicz Péter <antsticky@macbookair.t.hu>
2024-12-27 12:00:35 +0100

    add header getter endpoint

--- api/routes/stocks.py
+++ api/routes/stocks.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 
 
 from api.datamodels.api_inputs import InputAddStockModel
-from api.datamodels.api_responses import InsertTypeModel
+from api.datamodels.api_responses import InsertTypeModel, UniqueValuesHeaderTypeModel
 from api.misc.file_handler import FileHandler
 
-from api.middlewares.db_handlers import get_read_stockdb
+from api.middlewares.db_handlers import get_read_write_stockdb
 from api.middlewares.jwt_auth import get_jwt_payload_dependency
 
 load_dotenv()
@@ -30,7 +30,7 @@ async def add_stock(
     FileHandler.validate_file(stock_img)
     file_path = await FileHandler.save_file(stock_img, input.stock_type)
 
-    db = get_read_stockdb()
+    db = get_read_write_stockdb()
     collection = db["stocks"]
 
     try:
@@ -39,6 +39,24 @@ async def add_stock(
         )
         return InsertTypeModel(
             message="Entry added successfully", object_id=str(result.inserted_id)
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/stock/headers")
+async def get_header_names(
+    name: str,
+    _: dict = Depends(get_jwt_payload_dependency),
+) -> UniqueValuesHeaderTypeModel:
+
+    db = get_read_write_stockdb()
+    collection = db["stocks"]
+
+    try:
+        return UniqueValuesHeaderTypeModel(
+            message=f"Uniques values for column {name}",
+            unique_values=collection.distinct(name),
         )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
