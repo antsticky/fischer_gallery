9d8db0153dbe4427a8ef585e28fcfce161988abe
Elvagyok <elvagyok1@gmail.com>
2024-12-30 15:43:58 +0100

    fronten/frontend_init_with_nginx_and_yml_changes

---
+++ frontend/src/routes/basic.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Basic() {
+  return (
+    <div>
+      <h1>Basic</h1>
+      <h1>Basic 2</h1>
+      <h1>Basic 3</h1>
+    </div>
+  );
+}
+
+export default Basic;
